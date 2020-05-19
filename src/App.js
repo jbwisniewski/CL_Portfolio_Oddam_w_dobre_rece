f80b99f8ac51a97f427d5d77c83c59c0542e95c5
Jakub <38669542+jbwisniewski@users.noreply.github.com>
2020-05-19 17:09:10 +0200

    Dodanie komponentu Home, Navigation i pustych podkomponentów Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div className="App">Witamy</div>;
+  return (
+    <HashRouter>
+      <>
+        <Navigation />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </>
+    </HashRouter>
+  );
 }
 
 export default App;
